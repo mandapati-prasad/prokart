38f4296e27f6de46ca34d3fe009f94efbbd0c02a
durgaprasad <prasad.mandapati91@gmail.com>
2023-11-30 21:05:15 +0530

    edited server

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  app.get("/", (req, res) => {
+    res.send("welcome to the prokart website");
   });
 }
 
