717afe91c6604a6ce1ff914b435c199c1b9b29ef
durgaprasad <prasad.mandapati91@gmail.com>
2023-11-30 20:04:03 +0530

    modified serverjs

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-const cors = require("cors")
-
+const cors = require("cors");
 
 const app = express();
 // app.use(express.static("public"));
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-const path = require("path")
+const path = require("path");
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
 
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
 app.get("/", (req, res) => {
-  res.send("welcome to the prokart website")
-})
+  res.send("welcome to the prokart website");
+});
 
-const array = []
+const array = [];
 
 const calculateOrderAmount = (items) => {
   items.map((item) => {
-    const {price, cartQuantity} = item
+    const { price, cartQuantity } = item;
     const cartItemAmount = price * cartQuantity;
     return array.push(cartItemAmount);
-  })
-  const totalAmount = array.reduce((a,b) => {
-    return a+b
-  },0);
+  });
+  const totalAmount = array.reduce((a, b) => {
+    return a + b;
+  }, 0);
   return totalAmount * 100;
 };
 
@@ -47,17 +46,17 @@ app.post("/create-payment-intent", async (req, res) => {
     automatic_payment_methods: {
       enabled: true,
     },
-    shipping:{
-      address:{
-        line1:shipping.line1,
-        line2:shipping.line2,
-        city:shipping.city,
-        state:shipping.state,
-        country:shipping.country,
-        postal_code:shipping.postal_Code
+    shipping: {
+      address: {
+        line1: shipping.line1,
+        line2: shipping.line2,
+        city: shipping.city,
+        state: shipping.state,
+        country: shipping.country,
+        postal_code: shipping.postal_Code,
       },
-      name:shipping.name,
-      phone:shipping.phone
+      name: shipping.name,
+      phone: shipping.phone,
     },
     // receipt_email:userEmail
   });
@@ -67,5 +66,5 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 4242
+const PORT = process.env.PORT || 4242;
 app.listen(PORT, () => console.log(`Node server listening on port ${PORT}`));
