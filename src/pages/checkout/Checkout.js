eb03d4a0d8c968aea108779bc371622c06ccd8a2
durgaprasad <prasad.mandapati91@gmail.com>
2023-11-30 20:40:42 +0530

    latest edit

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -43,7 +43,7 @@ const Checkout = () => {
   useEffect(() => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("https://prokart.onrender.com/create-payment-intent", {
+    fetch("https://backend-mejg.onrender.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
